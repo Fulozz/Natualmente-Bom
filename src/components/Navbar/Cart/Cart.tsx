4b84d370d2b221f9b2c3c6fc711acfbd13f72959
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-01-31 17:45:52 -0300

    Criação e editor do Cart, e aquivo de utils

--- src/components/Navbar/Cart/Cart.tsx
+++ src/components/Navbar/Cart/Cart.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { Separator } from "@/components/ui/separator";
 import { Sheet, SheetTrigger,  SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { formatPrice } from "@/lib/utils";
+
 import { ShoppingCart } from "lucide-react"
 
 
 const Cart = () =>{
-    const itemCount = 0; 
+    const itemCount = 1; 
     return(
         <Sheet>
             <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -17,13 +20,33 @@ const Cart = () =>{
             <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
                 <SheetHeader className="space-y-2.5 pr-6">
                     <SheetTitle>Cart (0)</SheetTitle>
-                    Sheet
                 </SheetHeader>
                 { itemCount > 0 ? (
                     <>
-                    <div className="flex w-full flex-col pr-6">Card Items</div>
+                    {/* TODO: Cart logic */}
+                    <div className="flex w-full flex-col pr-6">Itens no carrinho</div>
+                    <div className="space-y-4 pr-6"> 
+                        <Separator />
+                        <div className="space-y-1.5 text-sm">
+                            <div className="flex">
+                                <span className="flex-1"> Taxa de entrega</span>
+                                {/* TODO: Taxa diminuir de acordo com a quantidade de encomenda até 100 reais, 10 reais de taxa, até 150 7 reais, acima de 200 gratuita */}
+                                <span>{formatPrice(10)}</span>
+                            </div>
+                        </div>
+                        <div className="space-y-1.5 text-sm">
+                            <div className="flex">
+                                <span className="flex-1"> Sem taxas adicionais</span>
+                                <span>{formatPrice(1)}</span>
+                            </div>
+                        </div>
+                    </div>
                     </>
-                ) : (<></>) }
+                ) : (
+                <div>
+
+                </div>
+                )}
             </SheetContent>
         </Sheet>
     )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// Formatação para ser exposto todo conteudo numerico em valor
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "BRL",
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+){
+  const {currency = "BRL", notation = "compact" } = options
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice)
+}
